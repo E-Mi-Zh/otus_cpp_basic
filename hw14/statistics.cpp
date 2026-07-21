bb1614879be2fc359d1dc5ca76d50a43977346c4
e.mi.zh <e.mi.zh@yandex.ru>
2026-07-22 00:12:37 +1000

    hw14: implement max, mean and std statistics

--- hw14/statistics.cpp
+++ hw14/statistics.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <cmath>
 
 class IStatistics {
 public:
@@ -12,7 +13,7 @@ public:
 
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::min()} {
+	Min() : m_min{std::numeric_limits<double>::max()} {
 	}
 
 	void update(double next) override {
@@ -33,14 +34,112 @@ private:
 	double m_min;
 };
 
+class Max : public IStatistics {
+public:
+	Max() : m_max{std::numeric_limits<double>::lowest()} {
+	}
+
+	void update(double next) override {
+		if (next > m_max) {
+			m_max = next;
+		}
+	}
+
+	double eval() const override {
+		return m_max;
+	}
+
+	const char * name() const override {
+		return "max";
+	}
+
+private:
+	double m_max;
+};
+
+class Mean : public IStatistics {
+public:
+	Mean() : m_mean{0.0}, m_count{0} {
+	}
+
+	// Используем алгоритм Уэлфорда https://habr.com/ru/articles/333426/
+	void update(double next) override {
+		m_count++;
+		m_mean = m_mean + ((next - m_mean) / m_count);
+	}
+
+	double eval() const override {
+		return m_mean;
+	}
+
+	const char * name() const override {
+		return "mean";
+	}
+
+private:
+	double m_mean;
+	size_t m_count;
+};
+
+// Для расчёта std воспользуемся тем же алгоритмом Уэлфорда https://habr.com/ru/articles/333426/
+// https://stackoverflow.com/questions/15638612/calculating-mean-and-standard-deviation-of-the-data-which-does-not-fit-in-memory/15638726#15638726
+class Std : public IStatistics {
+public:
+	Std() : m_m2{0.0}, m_count{0} {
+	}
+
+	void update(double next) override {
+        m_count++;
+
+		double old_mean = m_mean.eval();
+        m_mean.update(next);
+
+		double new_mean = m_mean.eval();
+        m_m2 = m_m2 + ((next - old_mean) * (next - new_mean));
+	}
+
+	double eval() const override {
+		// В задании не указано, что мы оцениваем: выборку или генеральную совокупность
+		// Исходя из того, что говорится о всей последовательности и в тексте задания слово
+		// "выборка" упоминается, будем считать, что имеется в виду СКО для генеральной совокупности
+		return std::sqrt(m_m2 / (m_count));
+		// Если вдруг понадобится СКО для выборки, то используем следующий код
+		// if (m_count < 2) {
+		//    // Если у нас один элемент, то его дисперсия (и СКО) равна нулю
+		//    return 0.0;
+		// }
+		// return std::sqrt(m_m2 / (m_count - 1));
+	}
+
+	const char * name() const override {
+		return "std";
+	}
+
+private:
+	double m_m2;
+	size_t m_count;
+	// Используем композицию для среднего
+	// гипотетически здесь могло быть приватное наследование
+	Mean m_mean;
+};
+
 int main() {
 
-	const size_t statistics_count = 1;
+	const size_t statistics_count = 4;
 	IStatistics *statistics[statistics_count];
 
 	statistics[0] = new Min{};
+	statistics[1] = new Mean{};
+	statistics[2] = new Std{};
+	statistics[3] = new Max{};
 
 	double val = 0;
+	// // для тестов
+	// for (val = 0; val < 11; val++) {
+	// 	std::cout << val << " ";
+	// }
+    // std::cout << std::endl;
+	// for (val = 0; val < 11; val++) {
 	while (std::cin >> val) {
 		for (size_t i = 0; i < statistics_count; ++i) {
 			statistics[i]->update(val);
